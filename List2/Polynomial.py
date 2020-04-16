5481a218ee95d964003ec4ff1ef3f1c40d9163d0
Piotrek1697 <30784285+Piotrek1697@users.noreply.github.com>
2020-04-16 23:36:36 +0200

    Polynomial multiply added

--- List2/Polynomial.py
+++ List2/Polynomial.py
@@ -11,27 +11,28 @@ class Polynomial:
                 the biggest degree
         """
         if len(coeffs) >= 2 and isinstance(coeffs, list) and (len(coeffs) - coeffs.count(0)) > 1:
-            self._arrayOfCoeffs = coeffs
+            self.arrayOfCoeffs = coeffs
         else:
-            raise Exception('Polynomial coefficients array must have at least two elements')
+            raise Exception('Polynomial coefficients array must have at least two elements and degree must be greater '
+                            'then 0')
 
     def polynomial_degree(self):
-        if self._arrayOfCoeffs[0] == 0:
-            return len(self._arrayOfCoeffs) - 2
+        if self.arrayOfCoeffs[0] == 0:
+            return len(self.arrayOfCoeffs) - 2
         else:
-            return len(self._arrayOfCoeffs) - 1
+            return len(self.arrayOfCoeffs) - 1
 
     def polynomial_pretty_print(self):
-        l = len(self._arrayOfCoeffs)
+        l = len(self.arrayOfCoeffs)
         for i in range(1, l + 1):
-            num = self._arrayOfCoeffs[i - 1]
+            num = self.arrayOfCoeffs[i - 1]
             sign = ''
             if num < 0:
                 sign = '-'
             elif num > 0 and i > 1:
                 sign = '+'
 
-            if num == 1 or num == -1:
+            if (num == 1 or num == -1) and i != l:
                 print(" %s x%d" % (sign, l - i), end='')
             elif num == 0:
                 print(end="")
@@ -42,5 +43,20 @@ class Polynomial:
                 print("*x%d" % (l - i), end='')
 
     def polynomial_value(self, x):
-        l = len(self._arrayOfCoeffs)
-        return sum([self._arrayOfCoeffs[i - 1] * pow(x, l - i) for i in range(1, l + 1)])
+        l = len(self.arrayOfCoeffs)
+        return sum([self.arrayOfCoeffs[i - 1] * pow(x, l - i) for i in range(1, l + 1)])
+
+    def polynomial_multiply(self, polynomial):
+        # Create list that length equals to greater degree in output polynomial
+        results = [0] * (self.polynomial_degree() + polynomial.polynomial_degree() + 1)
+        l = len(self.arrayOfCoeffs)
+        l2 = len(polynomial.arrayOfCoeffs)
+        for i in range(1, l + 1):
+            for j in range(1, l2 + 1):
+                num = self.arrayOfCoeffs[i - 1]
+                num2 = polynomial.arrayOfCoeffs[j - 1]
+
+                results[(l - i) + (l2 - j)] += num * num2
+
+        results.reverse()
+        return results
--- List2/Zad2.py
+++ List2/Zad2.py
@@ -4,12 +4,20 @@ from List2.Polynomial import Polynomial
 
 
 def main(args):
-    pol = [0,1,2]
+    pol = [2, 5, 1, 1, 6.24, 2, 2.3, 5.9, 1,23,2,5,8,1,789,2]
+    pol2 = [2, 4, 1,7,12,546]
     p = Polynomial(pol)
+    p2 = Polynomial(pol2)
+    print("Pierwszy")
     p.polynomial_pretty_print()
-    print()
-    print(p.polynomial_value(2))
+    print("\n*")
+    p2.polynomial_pretty_print()
+    print("\n||")
+    a = Polynomial(p.polynomial_multiply(p2))
+    a.polynomial_pretty_print()
+    print("\nWartość wielomianu:", p.polynomial_value(2))
     print("Stopień wielomianu:", p.polynomial_degree())
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
